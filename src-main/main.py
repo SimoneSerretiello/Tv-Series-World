633675dea4036455c0354fb3c68331167d67533e
MatteoToriello <95749843+MatteoToriello@users.noreply.github.com>
2021-12-18 12:26:20 +0100

    Update main.py

--- src-main/main.py
+++ src-main/main.py
@@ -7,25 +7,25 @@ from gaoperators import *
 
 def main():
     lista_serie=caricamentoDati()
-    lista_utente=carica_serie_utente(lista_serie)
-    lista, lista_codificata=inizializza(lista_serie)
+    lista_utente, generi=carica_serie_utente(lista_serie)
+    lista_serie=prepara_dataset(lista_serie, generi)
+    lista, lista_codificata=inizializza(lista_serie, lista_utente)
     lista_fit=fitness(lista_utente, lista, lista_codificata)
-    while num_generazioni(lista_fit)!=True:
+    while True:
         lista_codificata=twoTournament(lista_fit)
         lista_codificata=crossover(lista_codificata)
         lista_codificata=mutation_inv(lista_codificata)
         lista_fit=fitness(lista_utente, lista, lista_codificata)
-        
+        if stop(lista_fit)==True:
+            break 
     best=trova_result(lista_fit)
     print(stampa_result(lista_utente, lista, best))
 
         
-
-        
-
 
 
 if __name__=="__main__":
     main()
     inp=input()
     quit()
+
